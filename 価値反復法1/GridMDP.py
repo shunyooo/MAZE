c77c02059d1617f74c622f3c3616277aede9fa5c
Syunyo Kawamoto <syunyo@syunyooo-2.local>
2017-05-06 19:25:55 +0900

    ファイル整理  Q学習実装 モンテカルロ法途中

--- 価値反復法1/GridMDP.py
+++ 価値反復法1/GridMDP.py
@@ -2,7 +2,6 @@ from  MDP import MDP
 from utils import argmax, vector_add, print_table  # noqa
 from grid import orientations, turn_right, turn_left
 from pprint import pprint
-import numpy as np, pandas as pd
 
 import random
 
@@ -31,14 +30,19 @@ class GridMDP(MDP):
                 if self.state_check((y,x)):
                     self.states.add((y, x))
 
-    
+
+    # エピソードの終わりの判定
+    # 次状態を引数として枠外なら0を
     def state_check(self,state):
-           """ その状態が存在するかどうかを判定。"""
+           """エピソードの終わりの判定
+           次状態を引数として枠外なら0を返す。
+           """
            y,x = state
            if y < 0 or x < 0 or self.rows-1 < y or self.cols-1 < x :
                 return False
-           if self.grid[y][x] is None:# Noneだったら壁。
+           if self.reward[y,x] <= -100:#報酬が負だったら壁
                 return False
+
            return True
 
     # 状態stateで行動actionを取った時に、
@@ -71,16 +75,6 @@ class GridMDP(MDP):
         #print("state:{0}, direction:{1} -> state'{2}".format(state,direction,state1))
         return state1 if state1 in self.states else state
 
-    # 状態における報酬メソッドを変更。
-    # 状態と行動を引数に取ることにし、移動しない時の報酬を0にする。
-    # いままでは、その状態の価値を返していた。
-    def R(self, state, action):
-        n_state = self.go(state,action)
-        if n_state == state:
-            return 0.0
-        return self.reward[n_state]
-
-
     # 状態stateでとれる行動のリストを返す。
     # 問題用にオーバーライド。壁に向かう行動はできないようにする。
     def actions(self, state):
@@ -94,26 +88,85 @@ class GridMDP(MDP):
             return [a for a in self.actlist if self.state_check([x+y for (x,y) in zip(state,a)])]
 # ________________________________________________________________________________
 
+
+def value_iteration(mdp, epsilon=0.001):
+    """
+    入力としてGridMDPとepsilon:収束判定に用いる小さな値を受け取る。
+    出力として各状態における価値U(s)を返す。
+    
+    価値反復法
+    状態sから最適な行動をとり続けた時の期待利得を計算する。
+    得られる利得の期待値を考えるのは確率的な遷移を行われるため
+    1.全ての状態sについてU(s)を適当な値(ゼロなど)に初期化
+    2.全てのU(s)について以下の式を計算して値を更新
+      U(s) <- R(s) + r max sig[s'](P(s'|s,a)U(s'))
+    3.ステップ2が収束するまで繰り返す。
+    """
+
+    # 全ての状態sについてU(s)を0に初期化
+    U1 = {s: 0 for s in mdp.states}
+
+    # R(s):報酬関数,T(s,a):次状態への遷移確率と次状態のタプルを返す関数,gamma:割引係数
+    R, T, gamma = mdp.R, mdp.T, mdp.gamma
+    epoch = 0
+
+    while True:
+        U = U1.copy()
+        delta = 0
+        print("-"*40)
+        print(mdp.states)
+        printGrid(mdp.states,mdp.rows,mdp.cols)
+
+        #全ての状態sに関して
+        for s in mdp.states:
+            # 価値更新。
+
+            U1[s] = R(s) + gamma * max([sum([p * U[s1] for (p, s1) in T(s, a)])# (p:次状態への遷移確率,s1:次状態)
+                                        for a in mdp.actions(s)])# 状態sにおける、可能なaction全てに関して
+
+            print("s{0}".format(s))
+            printGrid(mdp.states,mdp.rows,mdp.cols,mark = s)
+            #この状態における全てのactionについて、
+            for a in mdp.actions(s):
+                print("----- a:{0}".format(toArrow(a)))
+                #遷移確率と、その状態
+                for p,s1 in T(s,a):
+                    print("---------- R[{0}]:{5} gamma:{6} p{3} U[{2}]{4}".format(s,a,s1,p,U[s1],R(s),gamma))
+
+
+
+            print(" U[{0}] = {1}".format(s,U[s]))
+            print("U1[{0}] = {1}".format(s,U1[s]))
+
+
+            delta = max(delta, abs(U1[s] - U[s]))# 変化値の最大値を取っておく
+            print("delta = {0}".format(delta))
+        # もし、変化値の最大値が閾値を超えなかったら->収束したら、この価値をreturn
+        if delta < epsilon: #* (1 - gamma) / gamma:
+            return U
+
+def best_policy(mdp, U):
+    """与えられたMDPと価値関数から最適な方策を決定する。
+    具体的には、全ての状態に関して{状態s:するべき行動a}の辞書を追加して返す。"""
+
+    pi = {}
+    for s in mdp.states:
+        # keyで比較を行なっている。つまり、sにおいて全ての(s,a)の価値を計算して比較。
+        # 最大のaを、{s:a}として登録。
+        pi[s] = argmax(mdp.actions(s), key=lambda a: expected_utility(a, s, U, mdp))
+    return pi
+
+# 与えられたMDPと価値関数Uから、s,aでの次状態s1での価値の期待値p*U(s1)の和
+# つまり、(s,a)における価値を表すっぽい。
+def expected_utility(a, s, U, mdp):
+    """The expected utility of doing a in state s, according to the MDP and U."""
+    return sum([p * U[s1] for (p, s1) in mdp.T(s, a)])
+
 # ______________________________________________________________________________
 
-def printGrid(grid,mark=None):
-    """gridを受け取って迷路の出力"""
-    wall = "■"
-    road = "□"
-    star = "☆"
-    for row in grid:
-        for i in row:
-            if i == None:
-                print(wall,end="")
-            # elif i == mark:
-            #     print(star,end="")
-            else:
-                print(road,end="")
-        print()
 
-
-def printGridByStates(tuples,rows,cols,mark=None):
-    """状態を受け取って、迷路の出力"""
+def printGrid(tuples,rows,cols,mark=None):
+    """迷路の出力"""
     wall = "■"
     road = "□"
     star_road = "☆"
